d3dd72b28b8b45809a88f612c6a034147b3a6468
Morgan Houston <mmhouston6@live.com>
2021-09-28 15:47:36 -0500

    Updated Player Prefab

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,15 +7,11 @@ public class MouseLook : MonoBehaviour
 {
 
     [SerializeField]private Vector2 mouseSensitivity;
-    [SerializeField]private float pitch; // y-axis
-    [SerializeField]private float yInput; // y-axis
-    [SerializeField]private float yaw; // x-axis
-    [SerializeField]private float xInput; // x-axis
-    [SerializeField]private float maxVerticalAngle;
+    [SerializeField]private float pitch, yaw, maxVerticalAngle;
+    private float yInput, xInput;
+    private float xRotation = 0f;
 
     public Transform playerBody;
-
-    private float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +20,7 @@ public class MouseLook : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
         yInput = pitch * mouseSensitivity.y * Time.deltaTime;
         xInput = yaw * mouseSensitivity.x * Time.deltaTime;
@@ -35,7 +30,6 @@ public class MouseLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * xInput);
-
     }
 
     public void Look(InputAction.CallbackContext context)
@@ -44,7 +38,6 @@ public class MouseLook : MonoBehaviour
         yaw = context.ReadValue<Vector2>().x;
     }
 
-    // Clamps angle between twgo angles.
     private float ClampVerticalAngle(float angle)
     {
         return Mathf.Clamp(angle, -maxVerticalAngle, maxVerticalAngle);
