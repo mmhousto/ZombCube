1ac5a911c0598d4822ccaabec3b2e123a65f7883
Morgan Houston <mmhouston6@live.com>
2024-06-23 23:10:30 -0500

    Increase Powerup Collider Radius. Can pause when dead in Party.

---
+++ Assets/Scripts/Networking/NetworkPauseManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Com.GCTC.ZombCube
+{
+    public class NetworkPauseManager : MonoBehaviour
+    {
+
+        public void OnGamePause(InputValue value)
+        {
+            Cursor.lockState = CursorLockMode.None;
+
+            NetworkGameManager.Instance.PauseGame();
+        }
+    }
+}
